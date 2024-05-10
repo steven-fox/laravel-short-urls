610d59cc18c97553e2a6d668239171e809f47a48
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2024-05-10 16:33:32 -0400

    use RefreshDatabase in Feature tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use StevenFox\Larashurl\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+uses(RefreshDatabase::class)->in('Feature');
